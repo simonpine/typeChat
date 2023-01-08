0b44fe4e8eaefef0b55294d34a86b5fea8502e2b
Simon Pineda <simonpineda0521@gmail.com>
2023-01-08 18:50:50 -0500

    the sign in error is solved

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -1,15 +1,13 @@
-// import firebase from "firebase/compat";
 import firebase from "firebase/compat/app";
 import { auth } from "../firebaseCom";
 function SignIn() {
-    const signInwithGoogle = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
-        const provider: firebase.auth.GoogleAuthProvider = new firebase.auth.GoogleAuthProvider()
+    function signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
         auth.signInWithPopup(provider)
-
     }
     return (
       <div className="App">
-        <button onClick={signInwithGoogle}>Sign in with google</button>
+        <button onClick={signInWithGoogle}>Sign in with google</button>
       </div>
     );
   }
